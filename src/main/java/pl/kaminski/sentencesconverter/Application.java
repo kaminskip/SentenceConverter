1b2d1413612ffc2ae956a18a9f31608cd58882a5
Pawel Kaminski <kaminski.pawel@atena.pl>
2015-12-04 08:11:55 +0100

    Add comment to main application

--- src/main/java/pl/kaminski/sentencesconverter/Application.java
+++ src/main/java/pl/kaminski/sentencesconverter/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by Paweł Kamiński.
+ * Main class to run application
  */
 @SpringBootApplication
 public class Application {
