63fc76eb867137e4bdb4d91ed34023b54077ba50
Pawel Kaminski <kaminski.pawel@atena.pl>
2015-12-07 14:56:04 +0100

    Refactoring after code coverage

---
+++ src/test/java/pl/kaminski/sentencesconverter/ApplicationTest.java
@@ -0,0 +1,16 @@
+package pl.kaminski.sentencesconverter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for Application main class
+ */
+public class ApplicationTest {
+
+    @Test
+    public void testMain() throws Exception {
+        Application.main(new String[]{});
+    }
+}
